8f538e47cc9ce7d7c1de93df301cbcaf76b33525
leojoli <leonardo.guns@gmail.com>
2024-03-25 10:34:14 -0300

    control-point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PointIdentificationComponent } from './point-identification/point-identification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PointIdentificationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/point-identification/point-identification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-identification',
+  templateUrl: './point-identification.component.html',
+  styleUrls: ['./point-identification.component.css']
+})
+export class PointIdentificationComponent {
+
+}
